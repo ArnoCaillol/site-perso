03cbf19ac86ffd36e8db19514e96a59b87627688
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-10-11 21:13:49 +0200

    feat : amélioration carousel dans projet

--- src/routes/projets/[slug]/+page.svelte
+++ src/routes/projets/[slug]/+page.svelte
@@ -1,5 +1,14 @@
 <script>
 	export let data;
+    let currentSlide = 1; // initial slide
+
+    function navigate(direction) {
+        if (direction === 'next') {
+            currentSlide = (currentSlide % 2) + 1;
+        } else if (direction === 'prev') {
+            currentSlide = ((currentSlide + 2) % 2) + 1;
+        }
+    }
 </script>
 <svelte:head>
 <title>Projet - {data.title}</title>
@@ -9,20 +18,26 @@
 	<h1>{data.title}</h1>
 	<p class="text-gray-500">Published: {data.date}</p>
 	<div class="carousel w-full">
+		{#if currentSlide === 1}
+
 		<div id="slide1" class="carousel-item relative w-full">
 			<img src="https://picsum.photos/500/200" class="w-full rounded-xl object-cover" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<a href="#slide2" class="btn btn-circle">❮</a> 
-				<a href="#slide2" class="btn btn-circle">❯</a>
+				<button on:click={() => navigate('prev')} class="btn btn-circle">❮</button>
+                <button on:click={() => navigate('next')} class="btn btn-circle">❯</button>
 			</div>
 		</div> 
+		{:else if currentSlide === 2}
+ 
 		<div id="slide2" class="carousel-item relative w-full">
 			<img src="https://picsum.photos/500/300" class="w-full rounded-xl object-cover" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<a href="#slide1" class="btn btn-circle">❮</a> 
-				<a href="#slide1" class="btn btn-circle">❯</a>
+				<button on:click={() => navigate('prev')} class="btn btn-circle">❮</button>
+                <button on:click={() => navigate('next')} class="btn btn-circle">❯</button>
 			</div>
 		</div>
+		{/if}
+
 	</div>
 	<svelte:component this={data.content} />
 </article>
