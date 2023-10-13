e60832a8c7ca1c5b8f0bc21aa256b7382c1f1c2f
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-10-13 11:35:49 +0200

    feat : boutons carousel

--- src/routes/projets/[slug]/+page.svelte
+++ src/routes/projets/[slug]/+page.svelte
@@ -1,40 +1,49 @@
 <script>
+	import emblaCarouselSvelte from 'embla-carousel-svelte'
+	
 	export let data;
-    let currentSlide = 1; // initial slide
-
-    function navigate(direction) {
-        if (direction === 'next') {
-            currentSlide = (currentSlide % 2) + 1;
-        } else if (direction === 'prev') {
-            currentSlide = ((currentSlide + 2) % 2) + 1;
-        }
-    }
+	let emblaApi
+	let options = { loop: true }
+	
+	const onInit = (event) => emblaApi = event.detail;
+	const scrollPrev = () => emblaApi.scrollPrev();
+	const scrollNext = () => emblaApi.scrollNext();
 </script>
+
 <svelte:head>
 <title>Arnaud Caillol - {data.title}</title>
 <meta property="og:title" content={data.title} />
 </svelte:head>
+
 <article class="prose lg:prose-lg mx-auto p-4 mb-8">
 	<h1>{data.title}</h1>
 	<p class="text-gray-500">Published: {data.date}</p>
-	<div class="carousel w-full">
-		{#if currentSlide === 1}
-		<div id="slide1" class="carousel-item relative w-full">
-			<img src="https://picsum.photos/500/200" class="w-full rounded-xl object-cover" />
-			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<button on:click={() => navigate('prev')} class="btn btn-circle">❮</button>
-                <button on:click={() => navigate('next')} class="btn btn-circle">❯</button>
-			</div>
-		</div> 
-		{:else if currentSlide === 2}
-		<div id="slide2" class="carousel-item relative w-full">
-			<img src="https://picsum.photos/500/300" class="w-full rounded-xl object-cover" />
-			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<button on:click={() => navigate('prev')} class="btn btn-circle">❮</button>
-                <button on:click={() => navigate('next')} class="btn btn-circle">❯</button>
+	<div class="flex items-center">
+		<button class="btn" on:click={scrollPrev}>❮</button>
+		<div class="embla" use:emblaCarouselSvelte="{{ options }}" on:emblaInit="{onInit}">
+			<div class="embla__container">
+				<div class="embla__slide flex items-center">
+					<img src="https://picsum.photos/500/200" class="w-full rounded-xl object-cover" />
+				</div>
+				<div class="embla__slide flex items-center">
+					<img src="https://picsum.photos/500/300" class="w-full rounded-xl object-cover" />
+				</div>
 			</div>
 		</div>
-		{/if}
+		<button class="btn" on:click={scrollNext}>❯</button>
 	</div>
 	<svelte:component this={data.content} />
 </article>
+
+<style>
+	.embla {
+		overflow: hidden;
+	}
+	.embla__container {
+		display: flex;
+	}
+	.embla__slide {
+		flex: 0 0 100%;
+		min-width: 0;
+	}
+</style>
