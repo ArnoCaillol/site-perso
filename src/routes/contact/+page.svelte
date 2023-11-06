88c4c390211dae922073b95655f91b3bbf4c576e
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-11-06 10:53:48 +0100

    feat : ajax form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,26 +1,27 @@
 <script>
-    // import { enhance } from '$app/forms';
-    
-    // export let form;
-    
-    // const handleEnhance = ({ formElement, formData, action, cancel }) => {
-    //     [...document.forms[0].elements].forEach(e => e.disabled = true);
+    const handleSubmit = (e) => {
+        [...document.forms[0].elements].forEach(e => e.disabled = true);
+        const myForm = e.target;
+        const formData = new FormData(myForm);
         
-    //     document.querySelector('.loading').classList.remove('opacity-0')
-    //     return async ({ result }) => {
-    //         [...document.forms[0].elements].forEach(e => e.disabled = true);
-    //         document.querySelector('.loading').classList.add('opacity-0');
-    //         if(result.type === 'success'){
-    //             const toastSuccess = document.getElementById('success');
-    //             toastSuccess.classList.remove('hidden');
-    //             setTimeout(() => toastSuccess.classList.add('hidden'), 3000);
-    //             [...document.forms[0].elements].forEach(e => e.disabled = false);
-    //             document.forms[0].reset();
-    //         } else {
-    //             const toastError = document.getElementById('error');
-    //         }
-    //     };
-    // }
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+        .then(() => {
+            document.forms[0].reset();
+            const toastSuccess = document.getElementById('success');
+            toastSuccess.classList.remove('hidden');
+            setTimeout(() => toastSuccess.classList.add('hidden'), 3000);
+        })
+        .catch((error) => {
+            const toastError = document.getElementById('error');
+            toastError.classList.remove('hidden');
+            setTimeout(() => toastError.classList.add('hidden'), 3000);
+        })
+        .complete(() => [...document.forms[0].elements].forEach(e => e.disabled = false));
+    };
 </script>
 
 <svelte:head>
@@ -32,21 +33,18 @@
     <div class="prose md:w-3/4 p-4">
         <h1 class="text-center">Contact</h1>
         <div class="card bg-base-200 mb-4">
-          <div class="card-body text-center">
-            <p>Utilisez le formulaire de contact ci-dessous</p>
-            <small>Vous pouvez également me joindre par téléphone ou sur les réseaux sociaux :</small>
-            <p class="text-center">
-                <a href="tel:+33646851070"><iconify-icon icon="mdi:phone" width="24" height="24"></iconify-icon></a> 
-                <a href="https://instagram.com/arnaud.caillol"><iconify-icon icon="mdi:instagram" width="24" height="24"></iconify-icon></a> 
-                <a href="https://www.linkedin.com/in/arnaud-caillol"><iconify-icon icon="mdi:linkedin" width="24" height="24"></iconify-icon></a> 
-                <a href="https://github.com/ArnoCaillol"><iconify-icon icon="mdi:github" width="24" height="24"></iconify-icon></a> 
-            </p>
-          </div>
+            <div class="card-body text-center">
+                <p>Utilisez le formulaire de contact ci-dessous</p>
+                <small>Vous pouvez également me joindre par téléphone ou sur les réseaux sociaux :</small>
+                <p class="text-center">
+                    <a href="tel:+33646851070"><iconify-icon icon="mdi:phone" width="24" height="24"></iconify-icon></a> 
+                    <a href="https://instagram.com/arnaud.caillol"><iconify-icon icon="mdi:instagram" width="24" height="24"></iconify-icon></a> 
+                    <a href="https://www.linkedin.com/in/arnaud-caillol"><iconify-icon icon="mdi:linkedin" width="24" height="24"></iconify-icon></a> 
+                    <a href="https://github.com/ArnoCaillol"><iconify-icon icon="mdi:github" width="24" height="24"></iconify-icon></a> 
+                </p>
+            </div>
         </div>
-        <!-- {#if form?.error}
-        <p class="error">{form.error}</p>
-        {/if} -->
-        <form name="contact" method="POST" netlify-honeypot="bot-field" netlify>
+        <form name="contact" method="POST" netlify-honeypot="bot-field" netlify on:submit|preventDefault={handleSubmit}>
             <input type="hidden" name="contact" value="contact">
             <div class="mb-6">
                 <label for="name">Nom</label>
@@ -65,11 +63,11 @@
         </form>
     </div>
 </div>
-<!-- <div class="toast toast-bottom toast-center">
+<div class="toast toast-bottom toast-center">
     <div id="success" class="alert alert-success hidden">
-        <span>Message envoyé avec succès !</span>
+        <span>Message envoyé !</span>
     </div>
     <div id="error" class="alert alert-error hidden">
         <span class="message">Erreur lors de l'envoi du message</span>
     </div>
-</div> -->
+</div>
