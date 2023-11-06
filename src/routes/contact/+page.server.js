21ed9a9b1f5d85ad2e35e96acf67740297c5ff0c
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-11-06 03:26:42 +0100

    fix : commentaire page.sever.js

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,28 +1,28 @@
-/** @type {import('./$types').Actions} */
-export const actions = {    
-    default: async ({ cookies, request }) => {
-        const data = await request.formData();
+// /** @type {import('./$types').Actions} */
+// export const actions = {    
+//     default: async ({ cookies, request }) => {
+//         const data = await request.formData();
 
-        fetch('https://formspree.io/f/xqkvwlzp', {
-        method: 'POST',
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            return { success: true };
-        } else {
-            response.json().then(data => {
-                if (Object.hasOwn(data, 'errors')) {
-                    console.log(data["errors"].map(error => error["message"]).join(", "))
-                } else {
-                    return { success: true };
-                }
-            })
-        }
-    }).catch(error => {
-        console.log("Oops! There was a problem submitting your form")
-    });
-}
-};
+//         fetch('https://formspree.io/f/xqkvwlzp', {
+//         method: 'POST',
+//         body: data,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }).then(response => {
+//         if (response.ok) {
+//             return { success: true };
+//         } else {
+//             response.json().then(data => {
+//                 if (Object.hasOwn(data, 'errors')) {
+//                     console.log(data["errors"].map(error => error["message"]).join(", "))
+//                 } else {
+//                     return { success: true };
+//                 }
+//             })
+//         }
+//     }).catch(error => {
+//         console.log("Oops! There was a problem submitting your form")
+//     });
+// }
+// };
