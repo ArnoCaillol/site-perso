3e35e170dcb46e85e04f8e1b384e5468b8dc03d9
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-11-10 21:29:16 +0100

    feat : aria-label pour les liens du footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,10 +4,10 @@
 <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <nav>
     <div class="grid grid-flow-col gap-4">
-      <a href="tel:+33646851070"><iconify-icon icon="mdi:phone" width="24" height="24"></iconify-icon></a> 
-      <a href="https://instagram.com/arnaud.caillol"><iconify-icon icon="mdi:instagram" width="24" height="24"></iconify-icon></a> 
-      <a href="https://www.linkedin.com/in/arnaud-caillol"><iconify-icon icon="mdi:linkedin" width="24" height="24"></iconify-icon></a> 
-      <a href="https://github.com/ArnoCaillol"><iconify-icon icon="mdi:github" width="24" height="24"></iconify-icon></a> 
+      <a href="tel:+33646851070" aria-label="Mon téléphone"><iconify-icon icon="mdi:phone" width="24" height="24"></iconify-icon></a> 
+      <a href="https://instagram.com/arnaud.caillol" aria-label="Mon compte Instagram"><iconify-icon icon="mdi:instagram" width="24" height="24"></iconify-icon></a> 
+      <a href="https://www.linkedin.com/in/arnaud-caillol" aria-label="Mon compte LinkedIn"><iconify-icon icon="mdi:linkedin" width="24" height="24"></iconify-icon></a> 
+      <a href="https://github.com/ArnoCaillol" aria-label="Mon compte GitHub"><iconify-icon icon="mdi:github" width="24" height="24"></iconify-icon></a> 
     </div>
   </nav> 
   <aside>
