427b6e881d5b57d9319b3b030b3ea32a16ee47a0
Arnaud Caillol <arnaud.caillol@tuta.io>
2023-12-14 15:52:02 +0100

    feat: "introduction" menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,12 +4,13 @@
 </script>
 <header class="sticky top-0 z-50 navbar bg-base-100 shadow-md">
 	<div class="flex-1">
-		<a aria-current={$page.url.pathname === '/' ? 'page' : undefined} href="/" class="btn btn-ghost normal-case text-xl"><img src="{logo}" width="47" height="47" alt="Arnaud Caillol">
+		<a href="/" class="btn btn-ghost normal-case text-xl"><img src="{logo}" width="47" height="47" alt="Arnaud Caillol">
 			<span class="text-xl font-bold ml-2">Arnaud Caillol</span>
 		</a>
 	</div>
 	<nav class="flex-none">
 		<ul class="menu menu-horizontal items-center px-1 hidden md:inline-flex">
+			<li><a aria-current={$page.url.pathname === '/' ? 'page' : undefined} href="/">Introduction</a></li>
 			<li><a aria-current={$page.url.pathname.startsWith('/projets') ? 'page' : undefined} href="/projets">Projets</a></li>
 			<li><a aria-current={$page.url.pathname.startsWith('/experiences') ? 'page' : undefined} href="/experiences">Expériences</a></li>
 			<li><a aria-current={$page.url.pathname.startsWith('/contact') ? 'page' : undefined} href="/contact">Contact</a></li>
@@ -19,7 +20,8 @@
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
 			</label>
 			<ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-				<li><a aria-current={$page.url.pathname.startsWith('/projets') ? 'page' : undefined} href="/projets">Projets</a></li>
+			<li><a aria-current={$page.url.pathname === '/' ? 'page' : undefined} href="/">Introduction</a></li>
+			<li><a aria-current={$page.url.pathname.startsWith('/projets') ? 'page' : undefined} href="/projets">Projets</a></li>
 				<li><a aria-current={$page.url.pathname.startsWith('/experiences') ? 'page' : undefined} href="/experiences">Expériences</a></li>
 				<li><a aria-current={$page.url.pathname.startsWith('/contact') ? 'page' : undefined} href="/contact">Contact</a></li>
 			</ul>
